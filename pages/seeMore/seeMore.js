709608e64c3f8e6b02ac0707d3d510dbee71677d
roycent <xingzhihuan@gmail.com>
2017-05-09 00:25:26 +0800

    增加按时间段查找日程功能
    ：）

--- pages/seeMore/seeMore.js
+++ pages/seeMore/seeMore.js
@@ -3,12 +3,16 @@ var that;
 var common = require('../template/getCode.js');
 var Bmob=require("../../utils/bmob.js");
 var vArray = [0,0,0,0,0];
+var itlist = new Array();
 Page({
   data:{
     varietyArray:[0,0,0,0,0],
     loading:false,
     windowHeight:0,
-    windowWidth:0
+    windowWidth:0,
+    startDate:"2017-01-01",
+    endDate:"2017-05-01",
+    itemList:[]
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
@@ -22,6 +26,7 @@ Page({
   },
   onShow:function(){
     // 页面显示
+    vArray = [0,0,0,0,0];
     var myInterval = setInterval(function(){
       wx.getStorage({
         key: 'user_id',
@@ -76,12 +81,92 @@ Page({
     })
   },
   onHide:function(){
-      vArray:[0,0,0,0,0]
+      vArray=[0,0,0,0,0];
+      itlist=[];
     // 页面隐藏
 
   },
   onUnload:function(){
     // 页面关闭
-      vArray:[0,0,0,0,0]
+      vArray=[0,0,0,0,0];
+      itlist=[];
+  },
+  setStartDate:function(e){
+    that.setData({
+      startDate:e.detail.value
+    })
+  },
+  setEndDate:function(e){
+    that.setData({
+      endDate:e.detail.value
+    })
+  },
+  searchDate:function(){
+    itlist=[];
+    var myInterval = setInterval(function () {
+      wx.getStorage({
+        key: 'user_id',
+        success: function (res) {
+          // success
+          if (res.data) {
+            clearInterval(myInterval);
+            var Schedule = Bmob.Object.extend("Schedule");
+            var query = new Bmob.Query(Schedule);
+            var isme = new Bmob.User();
+            isme.id = res.data;
+            query.equalTo("participant", isme);
+
+            query.find({
+              success: function (results) {
+                that.setData({
+                  loading: true
+                });
+                for (var i = 0; i < results.length; i++) {
+                  if(results[i].get("plannedDate")<=that.data.endDate && results[i].get("plannedDate")>=that.data.startDate)
+                  {
+                            var jsonA;
+                            var title=results[i].get("title");
+                            var content=results[i].get("content");
+                            var id=results[i].id;
+                            var created_at=results[i].createdAt;
+                            var _url;
+                            var pic=results[i].get("pic");
+                            var address=results[i].get("address");
+                            console.log(address);
+                            if(pic){
+                                jsonA='{"title":"'+title+'","content":"'+content+'","id":"'+id+'","created_at":"'+created_at+'","attachment":"'+pic._url+'","address":"'+address+'"}'
+                            }
+                            else{
+                              jsonA='{"title":"'+title+'","content":"'+content+'","id":"'+id+'","created_at":"'+created_at+'","address":"'+address+'"}'
+                            }
+                            var jsonB=JSON.parse(jsonA);
+                            itlist.push(jsonB)
+                            that.setData({
+                              itemList:itlist,
+                              loading: true
+                            })       
+                }      
+                }
+              },
+              error: function (error) {
+                common.loading(error, "loading");
+                that.setData({
+                  loading: false
+                });
+
+                console.log(error);
+              }
+            })
+          }
+        },
+        fail: function (res) {
+          // fail
+        },
+        complete: function (res) {
+          // complete
+        }
+      })
+    }, 500);
+    
   }
 })
